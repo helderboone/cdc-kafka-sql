b084db5c6b31dda90fb8d546a831253ffa851c60
helderboone <helder.boone@gmail.com>
2021-11-19 00:28:11 -0300

    Aumenta o tempo de sleep de executar o shell script

--- sql/import-data.sh
+++ sql/import-data.sh
@@ -1,4 +1,4 @@
 #aguardando 90 segundos para aguardar o provisionamento e start do banco
-sleep 20s
+sleep 25s
 #rodar o comando para criar o banco
 /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U SA -P "P@ssw0rd" -i criacao-banco-docker.sql
