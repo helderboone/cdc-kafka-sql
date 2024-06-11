f5c372c8a4b6d3a2c81f85f47bd027aa057b825f
Helder Borges <helder.borges@viavarejo.com.br>
2024-06-11 18:23:28 -0300

    Atualizacao do connector e inclusao de uma nova tabela Car

--- sql/criacao-banco-docker.sql
+++ sql/criacao-banco-docker.sql
@@ -21,6 +21,13 @@ CREATE TABLE Person (
     Phone VARCHAR(11)
 )
 
+CREATE TABLE Car (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Brand VARCHAR(100) NOT NULL,
+    Color VARCHAR(200) NOT NULL,
+    DateModification DateTime
+)
+
 PRINT N'Person table created successfully';
 
 -- Habilitar o CDC
@@ -37,11 +44,19 @@ EXEC sys.sp_cdc_enable_table
 @supports_net_changes = 1
 GO
 
+EXEC sys.sp_cdc_enable_table
+@source_schema = N'dbo',
+@source_name   = N'Car',
+@role_name     = N'Admin',
+@supports_net_changes = 1
+GO
+
 PRINT N'CDC enabled for Person Table';
 
 insert into dbo.Person values ('Joao Silva', 'Rua Augusta, 100', '9999999999');
-insert into dbo.Person values ('Marcos Santos', 'Rua Jorgina, 90', '9999999992');
-insert into dbo.Person values ('Andre Marcos Souza', 'Rua Antonio Pinto, 8778', '9999999993');
+insert into dbo.Car values ('Mercedez', 'Grey', GETDATE());
+-- insert into dbo.Person values ('Marcos Santos', 'Rua Jorgina, 90', '9999999992');
+-- insert into dbo.Person values ('Andre Marcos Souza', 'Rua Antonio Pinto, 8778', '9999999993');
 
 PRINT N'Data sample inserted';
 
